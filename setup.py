52c654b5a6e657f2e2ab1ac9cc568a66e9736f0a
gghioldi <gghioldi@yapstone.com>
2022-11-28 22:06:02 -0300

    changes in setup

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bind-sdk",
-    version="0.1.1",
+    version="0.2.0",
     author="Gustavo Ghioldi",
     author_email="gustavoghioldi@gmail.com",
     description="Easy integration with BIND Argentina",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gustavoghioldi/ghistylus",
+    url="https://github.com/gustavoghioldi/bind-sdk-python",
     packages=setuptools.find_packages(),
     python_requires=">=3",
     classifiers=[
